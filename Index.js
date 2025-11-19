99a1c876175890f3828be5af82e682a2ed46b883
youcode safi <kaoutar@gmail.com>
2025-11-19 10:29:11 +0100

    creation de function ExpDynamique qui pemet d'ajoyre et supprimer experience

--- Index.js
+++ Index.js
@@ -21,3 +21,44 @@ closeModal.addEventListener("click",()=>{
      modalOverlay.style.display="none"
 
 })
+
+
+
+function ExpDynamique(){
+    
+    AddExperiece.addEventListener("click",(e)=>{
+        e.preventDefault()
+    let Expprecedent = document.createElement('div')
+    Expprecedent.classList.add('Expperecedent')
+    Expprecedent.innerHTML=`<div class="group">
+                        <label for="post">Nom de post</label>
+                        <input id="post" class="post" required type="text" name="post">
+                    </div>
+                     <div class="group">
+                        <label for="periodeDeb">Date debut</label>
+                        <input id="periodeDeb"  class="Deb" required type="date" name="periodeDeb">
+                    </div>
+                    <div class="group">
+                        <label for="periodeDeb">Date fin</label>
+                        <input id="periodeFin" class="Fin" required type="date" name="periodeFin">
+                    </div>
+                    <div class="group">
+                        <label for="nameCompangie">Nom de la compagnie</label>
+                        <input id="nameCompangie" class="Compangie" required type="text" name="nameCompangie">
+                    </div>
+                    <div>
+                        <button class="btnSupprimer">Supprimer</button>
+                        
+                    </div>  `
+
+        ContainerExpe.appendChild(Expprecedent)
+     
+    const SuppExperience=Expprecedent.querySelector(".btnSupprimer")
+    SuppExperience.addEventListener("click",()=>{
+        Expprecedent.remove()
+
+    })
+    })
+
+
+}
